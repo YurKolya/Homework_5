e5bf1eb2eac614ac67668499539f5d933e3e3332
YurKolya <zinovjuri@yandex.ru>
2022-03-25 18:37:01 +0300

    Add Zinovyev_Yury_5_1.py

---
+++ Zinovyev_Yury_5_1.py
@@ -0,0 +1,16 @@
+""" task 1"""
+
+def odd_num(to):
+    for i in range(1, to + 1, 2):
+        yield i
+
+
+if __name__ == "__main__":
+    a_gen = odd_num(15)
+
+    print("a_gen type", type(a_gen))
+
+    for elem in a_gen:
+        print(elem)
+
+    print(f"empty {list(a_gen)}")
