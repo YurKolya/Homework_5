c3dac9542f4d84b012f4ff960741d8e71475876c
YurKolya <zinovjuri@yandex.ru>
2022-03-25 18:40:15 +0300

    Add Zinovyev_Yury_5_4.py

---
+++ Zinovyev_Yury_5_4.py
@@ -0,0 +1,23 @@
+""" task 4 """
+
+import time
+import sys
+
+
+def my_filter(*argv):
+    return(argv[i] for i in range(1, len(argv)) if argv[i] > argv[i - 1])
+    return (y for (x, y) in zip(argv[:-2], argv[1:]) if x < y)  # best
+    # return (argv[i] for i in range(1, len(argv)) if argv[i] > argv[i - 1])
+
+
+
+if __name__ == "__main__":
+
+    scr = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
+    result = [12, 44, 4, 10, 78, 123]
+
+    t = time.perf_counter()
+    answ = my_filter(*scr)
+    print(time.perf_counter() - t)
+    print(sys.getsizeof(answ))
+    print(list(answ) == result)  # we don't know assert for test
